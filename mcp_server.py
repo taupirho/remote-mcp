48791e6b8e8f7635ab81c134884a593694b104e6
taupirho <thomas.reid@gmail.com>
2025-06-25 10:26:32 +0100

    Update mcp_server.py

--- mcp_server.py
+++ mcp_server.py
@@ -18,7 +18,7 @@ app = FastAPI(lifespan=lifespan)
 app.mount("/stock", stock_mcp.streamable_http_app())
 app.mount("/flight", flight_mcp.streamable_http_app())
 
-PORT = 10000
+PORT = os.environ.get("PORT", 10000)
 
 if __name__ == "__main__":
     import uvicorn
